e042ec6a442b02f8573e0072ce623e59b9c2b783
Deepak Kumavat <deepakkumavat910@gmail.com>
2026-02-08 02:18:34 +0530

    Time: 4 ms (78.88%) | Memory: 128.3 MB (56.12%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> rearrangeArray(std::vector<int>& nums) {
+        int n = nums.size();
+        // Initialize a vector of size n with all elements as 0
+        std::vector<int> result(n, 0);
+
+        int posIndex = 0, negIndex = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                result[posIndex] = nums[i];
+                posIndex += 2;
+            } else {
+                result[negIndex] = nums[i];
+                negIndex += 2;
+            }
+        }
+    
+        return result;
+    }
+};

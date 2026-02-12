2e41a9c192bfd221b90e56647d00cd3b8201459c
Deepak Kumavat <deepakkumavat910@gmail.com>
2026-02-12 20:02:06 +0530

    Time: 54 ms (39.35%) | Memory: 32.8 MB (92.79%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -5,18 +5,17 @@ class Solution:
         
         nums.sort()
         longest = 1
-        current_length = 1
-        
-        for i in range(1, len(nums)):
-            # Skip duplicates
+        current = 1
+
+        for i in range(1,len(nums)):
             if nums[i] == nums[i-1]:
                 continue
             
-            # Check if consecutive
             if nums[i] - nums[i-1] == 1:
-                current_length += 1
+                current += 1
+
             else:
-                longest = max(longest, current_length)
-                current_length = 1
-        
-        return max(longest, current_length)
+                longest = max(longest, current)
+                current = 1
+
+        return max(longest,current)

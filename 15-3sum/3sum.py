bf2c8dfaeafb55c05ad4b67772d328b52071b1cd
Deepak Kumavat <deepakkumavat910@gmail.com>
2026-05-21 13:26:33 +0530

    Time: 629 ms (61.58%) | Memory: 22.4 MB (18.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,38 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         n = len(nums)
+        ans = []
         nums.sort()
 
         for i in range(n):
-            if i != 0 and nums[i] == nums[i - 1]:
+            if i!=0 and nums[i] == nums[i-1]:
                 continue
 
-            j = i + 1
-            k = n - 1
+            j=i+1
+            k=n-1
 
-            while j < k:
-                total_sum = nums[i] + nums[j] + nums[k]
+            while j<k:
+                ts = nums[i]+nums[j]+nums[k]
 
-                if total_sum < 0:
-                    j += 1
-                elif total_sum > 0:
-                    k -= 1
+                if ts<0:
+                    j+=1
+                elif ts>0:
+                    k-=1
                 else:
-                    temp = [nums[i], nums[j], nums[k]]
-                    ans.append(temp)   
-                    j += 1
-                    k -= 1
-
+                    temp = [nums[i],nums[j],nums[k]]
+                    ans.append(temp)
+                    j+=1
+                    k-=1
+                
+                    while j<k and nums[j] == nums[j-1]:
+                        j+=1
+                    while  j<k and nums[k]  == nums[k+1]:
+                        k-=1
                     
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
 
-        return ans   
+        return ans
+                
+
+
+        
+        

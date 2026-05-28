27a6f17b9adde48e3906e7940ca89f53b638c193
Deepak Kumavat <deepakkumavat910@gmail.com>
2026-05-28 11:21:24 +0530

    Time: 19 ms (74.42%) | Memory: 20 MB (64.37%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -6,10 +6,9 @@ class Solution:
 
             if stack and stack[-1] == ch:
                 stack.pop()
-            
+
             else:
                 stack.append(ch)
 
         return "".join(stack)
-
-            
+        

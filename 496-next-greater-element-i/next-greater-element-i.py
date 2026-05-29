8d14e9cc0da8bd97227c42a35fe21f5a1f8f5d5d
Deepak Kumavat <deepakkumavat910@gmail.com>
2026-05-30 00:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.63%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,19 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # so this approch goes like we take the map of every number in nums2 (the greater elements map)
         stack = []
         map = {}
 
         for i in nums2:
 
+            #mapping
             while stack and i > stack[-1]:
                 smaller = stack.pop()
                 map[smaller] = i
             
             stack.append(i)
-
+        #reamining numbers in stack 
         while stack:
             map[stack.pop()] = -1
         
-        return [map[i] for i in nums1]
+        return [map[i] for i in nums1] 
 
     

f09efb9e9ed8883e0c259486b42972064095dffe
Deepak Kumavat <deepakkumavat910@gmail.com>
2026-06-13 23:34:37 +0530

    Time: 6 ms (56.73%) | Memory: 22.8 MB (81.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,22 +1,31 @@
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        stack = []
-        ans = []
-        max_pair = 0
         freq = {}
 
         for i in nums:
-            freq[i] = freq.get(i,0)+1
+            freq[i] = freq.get(i, 0) + 1
 
-        for j in freq:
-            stack.append((freq[j], j))  # (frequency, actual number)
+        heap = []
 
-        while k:
-            max_pair = max(stack)        # max by frequency (first element of tuple)
-            ans.append(max_pair[1])      # append the actual number
-            stack.remove(max_pair)
-            k -= 1
+        for num, count in freq.items():
+
+            heapq.heappush(heap, (count, num))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        ans = []
+
+        while heap:
+            ans.append(heapq.heappop(heap)[1])
 
         return ans
+    
 
 
+
+        
+
+        
